5aa77a1006d55514b72c9c07a48e12538fa13d95
Moises Goncalves <moises@nimolabs.net>
2020-07-06 08:28:17 +0200

    updated the ui design

--- BuyBox/src/app/app.component.ts
+++ BuyBox/src/app/app.component.ts
@@ -1,3 +1,5 @@
+declare var jQuery: any;
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  total = 0;
   title = 'BuyBox';
+  addTokens(id: string): void  {
+    // jQuery('.toast').toast('show');
+    switch (id) {
+      case 'T010':
+        this.total = this.total + 10;
+        break;
+      case 'T020':
+        this.total += 20;
+        break;
+      case 'T050':
+        this.total += 50;
+        break;
+      default:
+        this.total += 100;
+        break;
+    }
+
+    this.total = this.total / 100;
+  }
+
+  buyProduct(id: number): void {
+    jQuery('#confirm').modal('show');
+  }
 }
